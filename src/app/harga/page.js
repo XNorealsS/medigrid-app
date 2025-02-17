d709451a64134fce072797d18210650e168ddad9
XNOREALSZ <muhammadrifaa973@gmail.com>
2025-02-17 16:16:13 +0700

    xnorealsz - 17-02-25 : 16:16

--- src/app/harga/page.js
+++ src/app/harga/page.js
@@ -1,87 +1,87 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/harga.module.css";
+import stylesx from "../styles/harga.module.css";
 
 
 const Harga = () => {
     return (
         <>
-        <nav className={styles["first-nav"]}>
-            <div className={styles['bahasa-faq']}>
+        <nav className={stylesx["first-nav"]}>
+            <div className={stylesx['bahasa-faq']}>
                 <p>IND <span></span></p>
                 <Link href="">FAQ</Link>
             </div>
-            <div className={styles['contact1']}>
+            <div className={stylesx['contact1']}>
                 <Link href="">Contact Us</Link>
             </div>
         </nav>
-    <section className={styles['container']}>
-        <nav className={styles['second-nav']}>
-            <div className={styles['navbar']}>
-                <div className={styles['logo']}>
+    <section className={stylesx['container']}>
+        <nav className={stylesx['second-nav']}>
+            <div className={stylesx['navbar']}>
+                <div className={stylesx['logo']}>
                   <Link href="/"><Image src="/img/medigrid.jpg" alt="Logo" width={140} height={100} /></Link>
                 </div>
-                <div className={styles['navigation']}>
-                    <Link href="/" className={styles['hove']}>Tentang Kami</Link>
-                    <Link href="/fitur" className={styles['hove']}>Fitur</Link>
-                    <Link href="/harga" className={`${styles['hove']} ${styles['active']}`}>Harga</Link>
-                    <Link href="" className={styles['hove']}>Blog</Link>
+                <div className={stylesx['navigation']}>
+                    <Link href="/" className={stylesx['hove']}>Tentang Kami</Link>
+                    <Link href="/fitur" className={stylesx['hove']}>Fitur</Link>
+                    <Link href="/harga" className={`${stylesx['hove']} ${stylesx['active']}`}>Harga</Link>
+                    <Link href="" className={stylesx['hove']}>Blog</Link>
                 </div>
             </div>
         </nav>
     </section>
-    <section className={styles['main']}>
-        <div className={styles['head']}>
+    <section className={stylesx['main']}>
+        <div className={stylesx['head']}>
             <header>
-                <div className={styles['header']}>
+                <div className={stylesx['header']}>
                     <h1>Harga</h1>
-                    <h2><span className={styles['trans']}>Tranparasi Harga</span> untuk Aplikasi <br/><span className={styles['klinik']}>Klinik Anda</span></h2>
+                    <h2><span className={stylesx['trans']}>Tranparasi Harga</span> untuk Aplikasi <br/><span className={stylesx['klinik']}>Klinik Anda</span></h2>
                     <p>MediGrid menyediakan harga yang kompetitif dan memastikan tidak akan ada biaya tersembunyi bagi pengguna. Paket yang ditawarkan mencakup fitur lengkap yang dirancang untuk meningkatkan produktivitas dan kualitas layanan kesehatan dari klinik Anda.</p>
                 </div>
             </header>
-           <div className={styles['list-harga']}>
-            <div className={styles['card1']}>
-                <div className={styles['list']}>
-                    <div className={styles['bulan']}>
+           <div className={stylesx['list-harga']}>
+            <div className={stylesx['card1']}>
+                <div className={stylesx['list']}>
+                    <div className={stylesx['bulan']}>
                         <h3>Bulanan</h3>
                         <p><strong>Rp <span>500.000</span></strong>/bln</p>
                     </div>
-                    <div className={styles['garis1']}></div>
-                    <div className={styles['bulan2']}>
+                    <div className={stylesx['garis1']}></div>
+                    <div className={stylesx['bulan2']}>
                         <h3>Atau setara dengan</h3>
                         <p><strong>Rp <span>6.000.000</span></strong>/bln</p>
                     </div>
-                    <button className={styles['btn']}>Berlangganan Sekarang</button>
+                    <button className={stylesx['btn']}>Berlangganan Sekarang</button>
                 </div>
             </div>
-                <div className={styles['card2']}>
-                    <div className={styles['list']}>
-                        <div className={styles['tahun']}>
+                <div className={stylesx['card2']}>
+                    <div className={stylesx['list']}>
+                        <div className={stylesx['tahun']}>
                             <h3>Tahunan</h3>
                             <p><strong>Rp <span>250.000</span></strong>/bln</p>
                         </div>
-                        <div className={styles['garis2']}></div>
-                        <div className={styles['tahun2']}>
+                        <div className={stylesx['garis2']}></div>
+                        <div className={stylesx['tahun2']}>
                             <h3>Atau setara dengan</h3>
                             <p><strong>Rp <span>3.000.000</span></strong>/bln</p>
                         </div>
-                         <button className={styles['btn']}>Berlangganan Sekarang</button>
+                         <button className={stylesx['btn']}>Berlangganan Sekarang</button>
                         </div>
                     </div>
                 </div>
-                <div className={styles['fitur']}>
-                    <div className={styles['judul']}>
+                <div className={stylesx['fitur']}>
+                    <div className={stylesx['judul']}>
                         <h1>Fitur</h1>
                     </div>
-                    <div className={styles['fitur-content']}>
+                    <div className={stylesx['fitur-content']}>
 
                     </div>
                 </div>
-                <div className={styles['fitur']}>
-                    <div className={styles['judul']}>
+                <div className={stylesx['fitur']}>
+                    <div className={stylesx['judul']}>
                         <h1>Fitur</h1>
                     </div>
-                    <div className={styles['fitur-content']}>
+                    <div className={stylesx['fitur-content']}>
 
                     </div>
                 </div>
@@ -92,51 +92,51 @@ const Harga = () => {
 
 
 
-    <footer className={styles['footer-body']}>
-        <div className={styles['footer-body2']}>
-            <div className={styles['alamat-logo']}>
-                <div className={styles['image-logo']}>
+    <footer className={stylesx['footer-body']}>
+        <div className={stylesx['footer-body2']}>
+            <div className={stylesx['alamat-logo']}>
+                <div className={stylesx['image-logo']}>
                     <Image src="/img/medigrid.jpg" alt="Logo" width={100} height={100}></Image>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ducimus quis, dolorem nulla pariatur excepturi eum, nesciu</p>
                 </div>
-                <div className={styles['media-sosial']}>
-                    <div className={styles['header-medsos']}>
+                <div className={stylesx['media-sosial']}>
+                    <div className={stylesx['header-medsos']}>
                         <h3>Media Sosial</h3>
                     </div>
-                    <div className={styles['medlog']}>
-                        <div className={styles['medsos-logo']}>
+                    <div className={stylesx['medlog']}>
+                        <div className={stylesx['medsos-logo']}>
                             <Link href=""><i className='bx bxl-instagram' ></i></Link>
                         </div>
-                        <div className={styles['medsos-logo']}>
+                        <div className={stylesx['medsos-logo']}>
                             <Link href="" ><i className='bx bxl-facebook' ></i></Link>
                         </div>
-                        <div className={styles['medsos-logo']}>
+                        <div className={stylesx['medsos-logo']}>
                             <Link href="" ><i className='bx bxl-twitter' ></i></Link>
                         </div>
-                        <div className={styles['medsos-logo']}>
+                        <div className={stylesx['medsos-logo']}>
                             <Link href=""><i className='bx bxl-youtube' ></i></Link>
                         </div>
                     </div>
                 </div>
-                <div className={styles['hubungi-kami']}>
-                    <div className={styles['hubungi']}>
+                <div className={stylesx['hubungi-kami']}>
+                    <div className={stylesx['hubungi']}>
                         <h3>Hubungi Kami</h3>
                     </div>
-                    <div className={styles['kami']}>
+                    <div className={stylesx['kami']}>
                         <Link href=""><i className='bx bxs-phone'></i>Hubungi kami</Link>
                         <Link href=""><i className='bx bxl-whatsapp' ></i>Hubungi kami</Link>
                         <Link href=""><i className='bx bx-envelope' ></i>Hubungi Kami</Link>
                     </div>
                         </div>
                     </div>
-                    <div className={styles['info-layan-bis-lain']}>
-                        <div className={styles['top-sections']}>
-                            <div className={styles['info-section']}>
-                                <header className={styles['head-info']}>
+                    <div className={stylesx['info-layan-bis-lain']}>
+                        <div className={stylesx['top-sections']}>
+                            <div className={stylesx['info-section']}>
+                                <header className={stylesx['head-info']}>
                                     <h3>Informasi Perusahaan</h3>
                                 </header>
-                                <div className={styles['list-info']}>
-                                    <ul className={styles['list']}>
+                                <div className={stylesx['list-info']}>
+                                    <ul className={stylesx['list']}>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
@@ -146,24 +146,24 @@ const Harga = () => {
                                     </ul>
                                 </div>
                             </div>
-                            <div className={styles['info-section']}>
-                                <header className={styles['head-info']}>
+                            <div className={stylesx['info-section']}>
+                                <header className={stylesx['head-info']}>
                                     <h3>Produk & Layanan</h3>
                                 </header>
-                                <div className={styles['list-info']}>
-                                    <ul className={styles['list']}>
+                                <div className={stylesx['list-info']}>
+                                    <ul className={stylesx['list']}>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
                                     </ul>
                                 </div>
                             </div>
-                            <div className={styles['info-section']}>
-                                <header className={styles['head-info']}>
+                            <div className={stylesx['info-section']}>
+                                <header className={stylesx['head-info']}>
                                     <h3>Bisnis & Pembaruan</h3>
                                 </header>
-                                <div className={styles['list-info']}>
-                                    <ul className={styles['list']}>
+                                <div className={stylesx['list-info']}>
+                                    <ul className={stylesx['list']}>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
@@ -171,13 +171,13 @@ const Harga = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className={styles['bottom-section']}>
-                            <div className={`${styles['info-section']} ${styles['lainnya']}`}>
-                                <header className={styles['head-info']}>
+                        <div className={stylesx['bottom-section']}>
+                            <div className={`${stylesx['info-section']} ${stylesx['lainnya']}`}>
+                                <header className={stylesx['head-info']}>
                                     <h3>Lainnya</h3>
                                 </header>
-                                <div className={styles['list-info']}>
-                                    <ul className={styles['list']}>
+                                <div className={stylesx['list-info']}>
+                                    <ul className={stylesx['list']}>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
                                         <li><Link href="">Tentang Kami</Link></li>
@@ -186,8 +186,8 @@ const Harga = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={styles['menu-footer']}>
-                        <div className={styles['list-menu']}>
+                    <div className={stylesx['menu-footer']}>
+                        <div className={stylesx['list-menu']}>
                             <Link href=""><i className='bx bx-location-plus'></i></Link>
                             <Link href=""><i className='bx bxs-phone'></i></Link>
                             <Link href=""><i className='bx bxs-contact' ></i></Link>
